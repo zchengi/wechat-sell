77610c1e23a14e764a74f065d1f962b71b47f2f4
cheng <792702352@qq.com>
2018-05-04 23:27:47 +0800

    :bug: 修复一个bug

--- src/main/java/com/cheng/vo/ProductVO.java
+++ src/main/java/com/cheng/vo/ProductVO.java
@@ -20,6 +20,6 @@ public class ProductVO {
     @JsonProperty("type")
     private Integer categoryType;
 
-    @JsonProperty("food")
+    @JsonProperty("foods")
     private List<ProductInfoVO> productInfoVOList;
 }
